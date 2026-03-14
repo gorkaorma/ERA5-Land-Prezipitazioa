1f7cf29d6ba7b6eb177e7961318dc2d1c41b7e6e
Gorka Orma <grkormaetxea@gmail.com>
2026-03-14 18:27:58 +0100

    Kodigo garbitu da eta tituloak gehitu dira

--- Europa-prezipitazioa.R
+++ Europa-prezipitazioa.R
@@ -4,6 +4,8 @@ library(ncdf4)        # Work with NetCDF metadata
 library(tidyverse)     # Data manipulation (filter, summarise, mutate)
 library(RColorBrewer) # Color palettes
 library(rnaturalearth)
+library(ggplot2)
+library(cowplot)
 
 # Set working directory
 setwd("~/R /ERA5 Land Prezipitazioa")
@@ -15,49 +17,55 @@ nc
 # Adjust the precipitation unit from m to mm (assuming the original unit is in meters)
 nc_mm <- nc * 1000  # Convertir de metros a milímetros
 
-# Plot the first valid time step (assuming the first layer corresponds to a valid time step)
-precip_raster <- nc_mm[[1]]  # Seleccionar la primera capa del raster
-
 # descargar países
 coast <- ne_coastline(scale = "medium", returnclass = "sv")
-precip_europa <- crop(precip_raster, coast)
+precip_europa <- crop(nc_mm, coast)
+
+# Find the max value in the cropped raster
+max_precip <- max(values(precip_europa), na.rm = TRUE)
+print(max_precip)
 
 # Crear paleta
 my_palette <- colorRampPalette(c("white", "lightblue", "darkblue"))
 cols <- my_palette(100)   # 100 colores
 
 # definir rangos fijos
-breaks_vals <- seq(0, 20, by = 2)
+breaks_vals <- seq(0, 14, by = 2)
 
 # Guardar plot como PNG
-png("precipitacion_urtarrila.png", width = 1200, height = 900, res = 150)
+png("precipitacion_2025.png", width = 1200, height = 1200, res = 300)
 
-# Plot the raster with minimal theme and remove the axis
-p1r <- plot(
-  precip_raster,
-  main = "Urtarrila",
-  breaks = breaks_vals,  # usar los breaks definido
-  col = cols,
-  axes = FALSE,  # Eliminar ejes
-  asp = 1,   # Mantener proporción 1:1
-  ) +
-  plot(coast,
-       add = TRUE,       # superponer sobre el raster
-       col = "black", # color del borde
-       lwd = 0.2 # grosor de línea
-  )
+# Layout 4 filas x 3 columnas
+layout(matrix(c(1:12,13,13,13), nrow=5, byrow=TRUE),
+       heights=c(1,1,1,1,0.3))
 
-# Guardar plot multiple time steps
-png("precipitacion_2025.png", width = 3600, height = 2600, res = 300)
+par(mfrow = c(4, 3),
+    mar = c(0.2, 0.2, 4.5, 0.2))  # Margen inferior, izquierdo, superior, derecho
 
-plot(nc_mm, 
-     main = "Precipitación mensual en Europa (2025)", 
-     col = cols, 
-     breaks = breaks_vals, 
-     axes = FALSE,
-     asp = 1
-    )
+mes_eu <- c(
+  "Urtarrila","Otsaila","Martxoa","Apirila",
+  "Maiatza","Ekaina","Uztaila","Abuztua",
+  "Iraila","Urria","Azaroa","Abendua"
+)
+
+for (i in 1:nlyr(nc_mm)) {
   
+  # raster del mes
+  plot(nc_mm[[i]],
+       breaks = breaks_vals,
+       col = cols,
+       axes = FALSE,
+       asp = 1,
+       legend = FALSE)
+  
+  # añadir costas
+  plot(coast,
+       add = TRUE,
+       col = "black",
+       lwd = 0.4)
+  
+  title(mes_eu[i], line = 3.0, cex.main=0.75, font.main=1)  # Título del mes
+}
 
 dev.off()  # Cerrar el dispositivo gráfico
 
